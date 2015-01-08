304adbeb8c7ad8c0c39e20d5cd2f09a2eaf70604
Aaron Hurley <aaronshurley@gmail.com>
2015-01-07 21:18:10 -0800

    completed 1.3

--- CTCI/arrays_and_strings.rb
+++ CTCI/arrays_and_strings.rb
@@ -9,9 +9,16 @@ end
 
 # 1.2
 def reverse_string!(str)
-  (str.length/2).times do |i|
+  (str.length / 2).times do |i|
     str[i], str[str.length - 1 - i] = str[str.length - 1 - i], str[i]
   end
 
   str
+end
+
+# 1.3
+class String
+  def is_permutation?(str2)
+    self.chars.sort == str2.chars.sort
+  end
 end
